fbe1bddf93076c207ee201292b7f6690ccc20d80
Ulisse Mini <ulisse.mini@gmail.com>
2019-10-19 15:11:39 -0400

    oh shit i'm an idiot

--- src/main/java/com/luamc/luamc/Player.java
+++ src/main/java/com/luamc/luamc/Player.java
@@ -3,9 +3,10 @@ package com.luamc.luamc;
 import org.luaj.vm2.LuaValue;
 import org.luaj.vm2.lib.jse.CoerceJavaToLua;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
 
 final class player {
-	static public LuaValue table() {
+	static public LuaValue table() {	
 		return CoerceJavaToLua.coerce(Minecraft.getMinecraft().player);
 	}
 }
